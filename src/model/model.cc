567782452c9f9a1ea563e8964698fbcef3551065
Onita Hyperion <ohyperio@student.21-school.ru>
2023-03-20 19:49:53 +0700

    feat: config.ini

--- src/model/model.cc
+++ src/model/model.cc
@@ -1,14 +1,34 @@
 #include "model.h"
+#include <QSettings>
 
 namespace vega {
 
 void Model::connectdb(QSqlDatabase *db) {
-  *db = QSqlDatabase::addDatabase("QMYSQL");
-  db->setDatabaseName("vega_db");
-  db->setHostName("172.17.0.2");
+
+//    QSettings settings("config.ini", QSettings::IniFormat);
+
+//    settings.setValue("mysql/host", "172.17.0.2");
+//    settings.setValue("mysql/user", "root");
+//    settings.setValue("mysql/password", "root");
+//    settings.setValue("mysql/database", "vega_db");
+
+
+    QSettings settings("config.ini", QSettings::IniFormat);
+
+    // Загружаем настройки
+    QString host = settings.value("mysql/host", "localhost").toString();
+    QString user = settings.value("mysql/user", "root").toString();
+    QString password = settings.value("mysql/password", "").toString();
+    QString database = settings.value("mysql/database", "test").toString();
+
+
+
+    *db = QSqlDatabase::addDatabase("QMYSQL");
+  db->setDatabaseName("database");
+  db->setHostName("host");
   db->setPort(3306);
-  db->setUserName("root");
-  db->setPassword("root");
+  db->setUserName("user");
+  db->setPassword("password");
   db->setConnectOptions("MYSQL_OPT_RECONNECT=1");
 }
 
